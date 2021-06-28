c947ce2a05024de56f01058191c81aa8638bb60e
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-06-28 12:28:42 +0200

    a

--- include/main.hpp
+++ include/main.hpp
@@ -1,4 +1,17 @@
 #pragma once
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <ph_network/network.hpp>
 
 
 auto main (int argc, char** argv) -> int;
+
+
+
+
+
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,35 @@
 
 
 
+
+
+
 auto main (int argc, char** argv) -> int 
 {
+    auto i = IP_v <4>;
+    
+    std::cout << "hello world" << std::endl;
+    
+    addrinfo hints
+    {
+        .ai_family = AF_UNSPEC,
+        .ai_socktype = SOCK_STREAM,
+        .ai_flags = AI_PASSIVE
+    };
+    
+    addrinfo *res;
+    
+    if (int err = getaddrinfo (nullptr, "3490", &hints, &res); err != 0)
+    {
+        fprintf (stderr, "getaddrinfo error: %s\n", gai_strerror (err));
+        exit (1);
 
+    }
+    
+    
+    freeaddrinfo (res);
 }
+
+
+
+
